08cd9ce216f3538f7692ebc1085112bbec20d083
AbdulmoizAbdulmaged <149574102+AbdulmoizAbdulmaged@users.noreply.github.com>
2024-04-27 17:53:17 +0300

    add all functions

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿
+using System;
+
+namespace WindowsFormsApp1
+{
+    partial class User
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "User Info";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.FullRowSelect = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 32);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(630, 311);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "User Id";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "User Name";
+            this.columnHeader2.Width = 175;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Email";
+            this.columnHeader3.Width = 197;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Password";
+            this.columnHeader4.Width = 120;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 350);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Save to text file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(748, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(151, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(748, 58);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(151, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(748, 84);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(151, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(748, 110);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(151, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(696, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "User id";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(676, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "User Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(704, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Email";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(683, 113);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Password";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(824, 145);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Add User";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(824, 174);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Edit User";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(824, 203);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Delete User";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(149, 350);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(179, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Push to snapchat";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // User
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(926, 375);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label1);
+            this.Name = "User";
+            this.Text = "Assignment app";
+            this.Load += new System.EventHandler(this.User_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void button2_Click_2(object sender, EventArgs e)
+        {
+          
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net.Http;
+using System.Text.Json;
+
+
+
+namespace WindowsFormsApp1
+{
+    public partial class User : Form
+    {
+        public User()
+        {
+            InitializeComponent();
+        }
+
+        public object ListView1 { get; private set; }
+       
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            //save data to text file
+            saveDataToTextFile();
+           
+        }
+
+       
+
+       
+       
+
+        private void User_Load(object sender, EventArgs e)
+        {
+            // read from text file
+            readUserInfoFromTextFile();
+           
+        }
+
+        
+
+        private void ListView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //get selected item
+            getSelectedItem();
+            
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+
+            //push to snapchat
+            pushToSnapchat();
+
+           
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            addUser();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //remove an item
+            removeUser();
+           
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //edit user
+            editUser();
+        }
+
+
+
+
+
+
+        //..............................................
+        public void pushToSnapchat()
+        {
+            //push the user's info to snapchat api
+            for (int i = 0; i < listView1.Items.Count; i++)
+            {
+                try
+                {
+                    var postData = new PostData // create user info object
+                    {
+                        UserName = listView1.Items[i].SubItems[1].Text,
+                        Email = listView1.Items[i].SubItems[2].Text,
+                        Password = listView1.Items[i].SubItems[3].Text
+
+
+                    };
+
+
+                    //passing the user info object to the snapchat api
+
+
+                    var client = new HttpClient();  //create http client object
+                    client.BaseAddress = new Uri("https://api.snapchat.com"); // provide api url, replace with actual snapchat url
+                    var json = JsonSerializer.Serialize(postData); // convert user info to json format
+                    var content = new StringContent(json, Encoding.UTF8, "application/json"); // add encoding type
+                    var response = client.PostAsync("/adplatform", content).Result; // send post request to api end point
+
+
+                    if (response.IsSuccessStatusCode) // check the reponse status
+                    {
+                        var responseContent = response.Content.ReadAsStringAsync().Result; // get the respone from api
+                        var postResponse = JsonSerializer.Deserialize<PostResponse>(responseContent); // convert from json to c# object
+
+                    }
+                    else
+                    {
+                        Console.WriteLine(response.StatusCode);
+                    }
+
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
+
+        }
+        //.................................
+        public void getSelectedItem()
+        {
+
+            // Check if an item is selected
+            if (listView1.SelectedItems.Count > 0)
+            {
+                // Get the index of the selected item
+                UserId.UserIndex = listView1.SelectedItems[0].Index;
+                textBox1.Text = listView1.SelectedItems[0].SubItems[0].Text;
+                textBox2.Text = listView1.SelectedItems[0].SubItems[1].Text;
+                textBox3.Text = listView1.SelectedItems[0].SubItems[2].Text;
+                textBox4.Text = listView1.SelectedItems[0].SubItems[3].Text;
+
+
+            }
+        }
+        //............................................
+        public void removeUser()
+        {
+            //remove an item
+            try
+            {
+                listView1.Items.RemoveAt(UserId.UserIndex);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        //..............................................
+        public void editUser()
+        {
+            //edit user
+            try
+
+            {
+                listView1.SelectedItems[0].SubItems[0].Text = textBox1.Text;
+                listView1.SelectedItems[0].SubItems[1].Text = textBox2.Text;
+                listView1.SelectedItems[0].SubItems[2].Text = textBox3.Text;
+                listView1.SelectedItems[0].SubItems[3].Text = textBox4.Text;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        //.................................................
+        public void readUserInfoFromTextFile()
+        {
+            try
+            {   // read the stored data from text file to listview
+                var fileLines = File.ReadAllLines(@"UserInfo.txt");
+                for (int i = 0; i + 3 < fileLines.Length; i += 4)
+                {
+                    listView1.Items.Add(
+                        new ListViewItem(new[]
+                        {
+                fileLines[i],
+                fileLines[i + 1],
+                fileLines[i + 2],
+                fileLines[i + 3],
+
+                        }));
+                }
+                // Subscribe to the SelectedIndexChanged event
+                listView1.SelectedIndexChanged += ListView1_SelectedIndexChanged;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+        }
+        //...........................................
+        public void saveDataToTextFile()
+        {
+            try
+            {
+                //save users info to text file
+                using (StreamWriter sw = new StreamWriter("UserInfo.txt"))
+                {
+                    for (int i = 0; i < listView1.Items.Count; i++)
+                    {
+                        sw.WriteLine(listView1.Items[i].SubItems[0].Text);
+                        sw.WriteLine(listView1.Items[i].SubItems[1].Text);
+                        sw.WriteLine(listView1.Items[i].SubItems[2].Text);
+                        sw.WriteLine(listView1.Items[i].SubItems[3].Text);
+                    }
+
+
+
+                }
+                MessageBox.Show("All User's Info have been saved successfully..");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+        }
+        //............................................
+        public void addUser()
+        {
+            try
+            {
+                //add single user info
+                ListViewItem item = new ListViewItem(textBox1.Text);
+                item.SubItems.Add(textBox2.Text);
+                item.SubItems.Add(textBox3.Text);
+                item.SubItems.Add(textBox4.Text);
+                listView1.Items.Add(item);
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox3.Clear();
+                textBox4.Clear();
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+    }
+    }
+    
+   
+
+
+
+
